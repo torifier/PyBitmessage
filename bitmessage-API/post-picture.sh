9c34a1090b3310a3633e0debb5c5458ef55029f1
torifier <alt.f7-boma8rsi@yopmail.com>
2016-07-14 10:24:17 +0000

    Create post-picture.sh

---
+++ bitmessage-API/post-picture.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+for file in ./payload/*
+do
+      ./BM-API-client.py  -e"${file}"   -s 'pic sent via API :   '  --es  --ttl='60*24*1'   -uUSER1 -pPASSWORD2
+#     echo "${file}"
+
+done
+
+#  send any binary file e.g. a picture via Bitmessage
+#  max. file size approx. 130 KBytes
+#
+#
+#  --embed ./binary-payloads/*
+#  multi pic post
+#
+#  'This text line will appear above the picture in the Bit-Message'
+# --en=1   vs.   for file in * do pp done
+
+# --eor=20 --eoc=99    --eory=100 --eorx=100    --edetect     ## did not quite work here...
+
+# 
+
+# --ttl 4 day default  TIME TO LIVE = storage time on BM before post will be dropped by BM network
+
+# or use clipboard method with Klipper
+#
+#
+#                -t BM-some-chan-address to post into
+#       Politics -t BM-2cVE8v7L4qb14R5iU2no9oizkx8MpuvRZ7
+#
